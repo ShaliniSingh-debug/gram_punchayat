932e81264304369ef5e82dce12dc2c594c1dfe7d
shalini singh <senger.shalinisingh@gmail.com>
2025-12-12 16:12:31 -0500

    added login section, dashboard and logout method to the users and forgot password functionality with hashed password

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from . import models, schemas
 from .database import engine
-from .routers import create_user , login, dashboard
+from .routers import create_user , dashboard, login
 
 
 models.Base.metadata.create_all(engine)
@@ -21,5 +21,4 @@ app = FastAPI()
 app.include_router(create_user.router)
 
 app.include_router(login.router)
-# app.include_router(dashboard.router)
 
